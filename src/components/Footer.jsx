48e6996cc95b628f12c2dd1fb4a17cf9fb52e841
abdellah <abdellahalioua200@gmail.com>
2025-10-14 22:38:33 +0100

    Update Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
       <div className="container mx-auto">
         <p>&copy; {currentYear} Abdellah Alioua. All rights reserved.</p>
         <div className="flex justify-center space-x-4 mt-4">
-          <a href="https://github.com/YourGitHub" target="_blank" rel="noopener noreferrer" className="text-[#b3b3b3] hover:text-[#ff4d5a]">
+          <a href="https://github.com/ABDELLAH-H2" target="_blank" rel="noopener noreferrer" className="text-[#b3b3b3] hover:text-[#ff4d5a]">
             <i className="fab fa-github text-xl"></i>
           </a>
-          <a href="https://linkedin.com/in/YourLinkedIn" target="_blank" rel="noopener noreferrer" className="text-[#b3b3b3] hover:text-[#ff4d5a]">
+          <a href="https://www.linkedin.com/in/abdellah-alioua-7b8289384/" target="_blank" rel="noopener noreferrer" className="text-[#b3b3b3] hover:text-[#ff4d5a]">
             <i className="fab fa-linkedin text-xl"></i>
           </a>
           {/* Add more social links */}
